7c8d4db1ca0b3f44873dda62dfe66d9780b7f6f1
nairansilva <31315937+nairansilva@users.noreply.github.com>
2022-06-07 18:22:08 -0300

    Criação de API para retornar um gráfico utilizado no dash

--- src/app/components/dash/dash.component.ts
+++ src/app/components/dash/dash.component.ts
@@ -1,3 +1,4 @@
+import { DashService } from './shared/dash.service';
 import { Component, OnInit } from '@angular/core';
 import { PoChartOptions, PoChartSerie, PoChartType } from '@po-ui/ng-components';
 
@@ -8,29 +9,38 @@ import { PoChartOptions, PoChartSerie, PoChartType } from '@po-ui/ng-components'
 })
 export class DashComponent implements OnInit {
 
-  options: PoChartOptions = {
-    axis: {
-      minRange: 0,
-      maxRange: 40,
-      gridLines: 5
-    }
-  };
+  loading = true;
+  title = '';
+  options: PoChartOptions;
 
   participationByCountryInWorldExportsType: PoChartType = PoChartType.Line;
 
-  categories: Array<string> = ['2010', '2011', '2012', '2013', '2014', '2015'];
+  categories: Array<string>;// = ['2010', '2011', '2012', '2013', '2014', '2015'];
+  participationByCountryInWorldExports: Array<PoChartSerie>;
+  // participationByCountryInWorldExports: Array<PoChartSerie> = [
+  //   { label: 'Brazil', data: [35, 32, 25, 29, 33, 33], color: 'color-10' },
+  //   { label: 'Vietnam', data: [15, 17, 23, 19, 22, 18] },
+  //   { label: 'Colombia', data: [8, 7, 6, 9, 10, 11] },
+  //   { label: 'India', data: [5, 6, 5, 4, 5, 5] },
+  //   { label: 'Indonesia', data: [7, 6, 10, 10, 4, 6] }
+  // ];
 
-  participationByCountryInWorldExports: Array<PoChartSerie> = [
-    { label: 'Brazil', data: [35, 32, 25, 29, 33, 33], color: 'color-10' },
-    { label: 'Vietnam', data: [15, 17, 23, 19, 22, 18] },
-    { label: 'Colombia', data: [8, 7, 6, 9, 10, 11] },
-    { label: 'India', data: [5, 6, 5, 4, 5, 5] },
-    { label: 'Indonesia', data: [7, 6, 10, 10, 4, 6] }
-  ];
-
-  constructor() { }
+  constructor(private dashService:DashService) {
+    this.dashService.getModulos().subscribe(
+      res => {
+        this.title = res.title;
+        this.options = {axis:res.options};
+        this.categories = res.categories;
+        this.participationByCountryInWorldExports = res.series;
+        console.log(this.options);
+        this.loading = false;
+      },
+      erro => console.log(erro)
+    )
+   }
 
   ngOnInit() {
+
   }
 
 }
---
+++ src/app/components/dash/shared/dash.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'content-Type': 'application/json' })
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  getModulos(): Observable<any>{
+    const url = "http://localhost:8080/rest/api/treinamento/v1/consultaModulos"
+    return this.httpClient.get<any>(`${url}`,)
+  }
+}
